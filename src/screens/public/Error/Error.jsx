4ab9f5c5eba93930d9ae6d4cf4ef242f83cbf792
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-05-07 20:29:08 -0300

    refactor(ErrorScreen): Ajustada exibição dos botões e textos

--- src/screens/public/Error/Error.jsx
+++ src/screens/public/Error/Error.jsx
@@ -5,11 +5,16 @@ import styles from './styles';
 const ErrorScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>
-        Erro! Essa página não está disponível
+      <Text style={styles.errorText}>
+        Erro!
       </Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('home')}>
-        <Text style={styles.buttonText}>Voltar para o início</Text>
+      <Text style={styles.descriptionText}>
+        Essa página não está disponível
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          Voltar para o início
+        </Text>
       </TouchableOpacity>
     </View>
   );
