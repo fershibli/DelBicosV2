6ae210e0bb92f4b994166ff8feeae017021d6bf8
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-05-07 20:09:14 -0300

    feat(ErrorScreen): Definido primeiros elementos

---
+++ src/screens/public/Error/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import styles from './styles';
+
+const ErrorScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Erro! Essa página não está disponível</Text>
+    </View>
+  );
+};
+
+
+export default ErrorScreen;
