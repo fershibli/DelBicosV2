1b5820086530a5c84e4fff0e9211272982e8b1b2
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-05-14 19:54:15 -0300

    feat(ErrorScreen): Atualiza a tela de erro com novos estilos e estrutura

--- src/screens/public/Error/styles.jsx
+++ src/screens/public/Error/styles.jsx
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
       width: '100%',
       height: '100%',
       backgroundColor: '#FC8200',
-      borderRadius: 45,
+      borderRadius: 0,
       justifyContent: 'center',
       alignItems: 'center',
     },
