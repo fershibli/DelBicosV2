8b22702886128135ce18d4d94c8fabe418d79ba7
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-05-14 21:15:23 -0300

    feat(Index): substitui conteúdo padrão por tela de status de serviços

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import styles from "../screens/public/ServicesStatus/styles"
+import { TouchableOpacity} from "react-native-gesture-handler"
+import ServiceStatusScreen from "../screens/public/ServicesStatus/ServiceStatusScreen ";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <ServiceStatusScreen />
     </View>
   );
 }
