7f2eeae37adadc11622cb511a0f10c40833fe8d6
douglaswenzel <douglas.wenzel@hotmail.com>
2025-05-26 14:58:51 -0300

    feat(SobreContent): exibição das informações de Detalhes e Comodidaes oferecidas pelo parceiro

---
+++ src/screens/public/PartnerProfile/SobreContent.tsx
@@ -0,0 +1,68 @@
+import { Button, Text } from '@react-navigation/elements';
+import { StyleSheet, View } from 'react-native';
+
+export function SobreContent() {
+  return (
+  <View style={styles.contentContainer}>
+    <View style={styles.section}>
+      <Text style={styles.sectionTitle}>Detalhes</Text>
+      <Text style={styles.sectionText}>Não informado</Text>
+    </View>
+
+    <View style={styles.section}>
+      <Text style={styles.sectionTitle}>Comodidades</Text>
+      <View style={styles.list}>
+        <Text style={styles.listItem}>• Acessibilidade à deficientes</Text>
+        <Text style={styles.listItem}>• Atendimento à crianças</Text>
+        <Text style={styles.listItem}>• Wifi</Text>
+      </View>
+    </View>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  navTabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#E0E0E0',
+  },
+  tab: {
+    fontSize: 17,
+    color: '#666',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+  activeTab: {
+    color: '#FC8200',
+    fontWeight: 'bold',
+    borderBottomWidth: 2,
+    borderBottomColor: '#FC8200',
+  },
+  contentContainer: {
+    padding: 16,
+  },
+  section: {
+    marginBottom: 24,
+  },
+  sectionTitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: '#333',
+  },
+  sectionText: {
+    fontSize: 15,
+    color: '#666',
+  },
+  list: {
+    marginLeft: 16,
+  },
+  listItem: {
+    fontSize: 15,
+    color: '#666',
+    marginBottom: 4,
+  },
+});
