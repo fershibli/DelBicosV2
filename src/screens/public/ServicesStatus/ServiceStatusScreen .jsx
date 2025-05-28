4960815184c5e897401b57f8e942efa790186ebd
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-05-27 21:51:09 -0300

    feat(ServiceStatusScreen): adiciona mock de serviços para renderização primitiva

--- src/screens/public/ServicesStatus/ServiceStatusScreen .jsx
+++ src/screens/public/ServicesStatus/ServiceStatusScreen .jsx
@@ -7,6 +7,27 @@ import ServiceItems from '@components/ServiceItems';
 import PaymentInfo from '@components/PaymentInfo';
 import styles from '@components/styles';
 
+const mockServices = [
+  {
+    id: '1',
+    name: 'Barba',
+    date: '5/12/2024',
+    startTime: '14:30',
+    endTime: '15:00',
+    price: 38.00,
+    professional: 'Jefferson'
+  },
+  {
+    id: '2',
+    name: 'Corte de Cabelo',
+    date: '5/12/2024',
+    startTime: '15:00',
+    endTime: '15:45',
+    price: 45.00,
+    professional: 'Jefferson'
+  }
+];
+
 const ServiceStatusScreen = () => {
   return (
     <View style={styles.container}>
