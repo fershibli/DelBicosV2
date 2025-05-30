00d610e2ab7f34788695009de7f724528804819d
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-05-29 21:28:52 -0300

    refactor(ErrorScreen): melhorar formatação do código e remover quebras de linha desnecessárias

--- src/screens/public/Error/Error.jsx
+++ src/screens/public/Error/Error.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
 const ErrorScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.errorText}>
-        Erro!
-      </Text>
+      <Text style={styles.errorText}>Erro!</Text>
       <Text style={styles.descriptionText}>
         Essa página não está disponível
       </Text>
       <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>
-          Voltar para o início
-        </Text>
+        <Text style={styles.buttonText}>Voltar para o início</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
 
 export default ErrorScreen;
