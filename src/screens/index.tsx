c2be9e076022333c8254679d01bbcd7f528d2d2b
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-04 15:42:26 -0300

    refactor: remove Profile and Updates screens from the project

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -5,12 +5,9 @@ import {
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Image } from 'react-native';
-import bell from '../assets/bell.png';
 import newspaper from '../assets/newspaper.png';
 import { Loading } from './LoadingScreen';
 import Home from './public/Home';
-import { Profile } from './Profile';
-import { Updates } from './Updates';
 import NotFound from './public/NotFound';
 import PhoneConfirmation from './public/PhoneConfirmation';
 import ConfirmPhoneNumber from './public/ConfirmPhoneNumber';
@@ -18,65 +15,10 @@ import RegisterScreen from './public/RegisterScreen';
 import PartnerProfile from './public/PartnerProfile';
 import ServiceStatusScreen from './public/ServicesStatus/ServiceStatusScreen ';
 
-const HomeTabs = createNativeStackNavigator({
-  screens: {
-    Home: {
-      screen: Loading,
-      options: {
-        headerShown: false,
-      },
-    },
-  },
-});
-
-const TabNavigator = createBottomTabNavigator({
+const RootStack = createNativeStackNavigator({
   screens: {
     Home: {
       screen: Home,
-      options: {
-        title: 'Feed',
-        headerShown: false,
-        tabBarIcon: ({ color, size }) => (
-          <Image
-            source={newspaper}
-            tintColor={color}
-            style={{
-              width: size,
-              height: size,
-            }}
-          />
-        ),
-      },
-    },
-    Updates: {
-      screen: Updates,
-      options: {
-        tabBarIcon: ({ color, size }) => (
-          <Image
-            source={bell}
-            tintColor={color}
-            style={{
-              width: size,
-              height: size,
-            }}
-          />
-        ),
-      },
-    },
-  },
-});
-
-const RootStack = createNativeStackNavigator({
-  screens: {
-    HomeTabs: {
-      screen: HomeTabs,
-      options: {
-        title: 'Home',
-        headerShown: false,
-      },
-    },
-    Home: {
-      screen: TabNavigator,
       options: {
         headerShown: false,
       },
@@ -97,18 +39,6 @@ const RootStack = createNativeStackNavigator({
       screen: ServiceStatusScreen,
       linking: {
         path: 'service-status',
-      },
-    },
-    Profile: {
-      screen: Profile,
-      linking: {
-        path: ':user(@[a-zA-Z0-9-_]+)',
-        parse: {
-          user: (value: string) => value.replace(/^@/, ''),
-        },
-        stringify: {
-          user: (value: string) => `@${value}`,
-        },
       },
     },
     PhoneConfirmation: {
