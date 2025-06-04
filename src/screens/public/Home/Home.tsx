ff70ba4d14fa36d724b305d9e43023f8948626fa
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-04 15:24:16 -0300

    refactor(Home): move code to src/screens/public and split screen and style into diferent files

--- src/screens/public/Home/Home.tsx
+++ src/screens/public/Home/Home.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  ScrollView,
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-} from 'react-native';
-import { useLocation } from '../lib/util/LocationContext';
+import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
+import { useLocation } from '../../../lib/util/LocationContext';
+import { styles } from './styles';
 
 // Mock de dados para o feed de "bicos"
 const bicos = [
@@ -30,7 +25,7 @@ const bicos = [
   },
 ];
 
-export function Home() {
+function HomeScreen() {
   const { city, state } = useLocation();
 
   const handleBicoDetails = (bicoId: number) => {
@@ -68,73 +63,4 @@ export function Home() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e6f0fa',
-  },
-  header: {
-    backgroundColor: '#003366',
-    padding: 15,
-    alignItems: 'center',
-  },
-  headerText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#ffffff',
-  },
-  contentContainer: {
-    alignItems: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#003366',
-    marginBottom: 20,
-  },
-  card: {
-    backgroundColor: '#ffffff',
-    borderRadius: 8,
-    padding: 15,
-    width: '100%',
-    marginBottom: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
-  },
-  cardTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#003366',
-    marginBottom: 5,
-  },
-  cardDescription: {
-    fontSize: 14,
-    color: '#003366',
-    marginBottom: 10,
-  },
-  cardLocation: {
-    fontSize: 12,
-    color: '#003366',
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: '#ff7f00',
-    padding: 10,
-    borderRadius: 8,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#ffffff',
-    fontWeight: 'bold',
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 10,
-    fontSize: 12,
-    color: '#003366',
-  },
-});
+export default HomeScreen;
