c75f3d97e17598fec3cf0bd72a0cdf73de80e177
douglaswenzel <douglas.wenzel@hotmail.com>
2025-06-05 13:06:40 -0300

    hotfix(Header): remove unused header styles and clean up style definitions

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,18 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  header: {
-    marginTop: 20,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-    justifyContent: 'flex-start',
+  container: {
+    width: '100%',
+    backgroundColor: '#FFFFFF',
   },
-  headerText: {
-    fontWeight: '700',
-    fontSize: 15,
-    lineHeight: 18,
-    color: '#005A93',
+  topSection: {
+    paddingVertical: 20,
+    paddingLeft: 20,
+  },
+  logo: {
+    width: 476,
+    height: 145,
   },
 });
