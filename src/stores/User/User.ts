2ace5a9653186268795940f87f52c986ae49124f
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-17 17:31:30 -0300

    refactor(stores/User): change export to default and create index file for User store

--- src/stores/User/User.ts
+++ src/stores/User/User.ts
@@ -1,8 +1,10 @@
 import { create } from 'zustand';
 import { UserStore } from './types';
 
-export const useUserStore = create<UserStore>((set) => ({
+const useUserStore = create<UserStore>((set) => ({
   user: null,
   setUser: (user) => set({ user }),
   logout: () => set({ user: null }),
 }));
+
+export default useUserStore;
