4ff60704da1c4930e2474e4f6eeb0aeec803f92a
douglaswenzel <douglas.wenzel@hotmail.com>
2025-06-17 20:56:19 -0300

    feat: initial files for creating the screen

---
+++ src/screens/public/MyAppointments/MyAppointments.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+
+const MyAppointments = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+};
+
+export default MyAppointments;
