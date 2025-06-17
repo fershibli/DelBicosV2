cdf568acd1d900a6cf733359d529b815c978991d
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-17 17:31:55 -0300

    refactor(Header): update import for useUserStore and clean up code formatting

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,25 +4,23 @@ import { styles } from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import NavItem from './styles';
 import { useNavigation } from '@react-navigation/native';
-import { useUserStore } from '@stores/userStore'; 
+import useUserStore from '@stores/User';
 
 type Props = {
   isAuthenticated: boolean;
 };
 
-
-
 const Header = ({ isAuthenticated }: Props) => {
-
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-
-      <TouchableOpacity style={styles.topSection} onPress={() => navigation.navigate('Home')}>
-        <Image 
-          source={require('../../assets/DelBicos_LogoH.png')} 
-          style={styles.logo} 
+      <TouchableOpacity
+        style={styles.topSection}
+        onPress={() => navigation.navigate('Home')}>
+        <Image
+          source={require('../../assets/DelBicos_LogoH.png')}
+          style={styles.logo}
           resizeMode="contain"
         />
       </TouchableOpacity>
@@ -34,7 +32,6 @@ const Header = ({ isAuthenticated }: Props) => {
       </View>
 
       <View style={styles.blueBar} />
-
     </View>
   );
 };
@@ -43,22 +40,25 @@ const AuthenticatedNav = () => {
   const user = useUserStore((state) => state.user);
 
   return (
-  <View style={styles.navItems}>
-    <NavItem>Página Inicial</NavItem>
-    <NavItem>Categorias</NavItem>
-    <NavItem>Ajuda</NavItem>
-    <NavItem>Meus Agendamentos</NavItem>
-    <NavItem>Portal do Parceiro</NavItem>
-    <View style={styles.locationContainer}>
-      <Text style={styles.locationLabel}>Estou em:</Text>
+    <View style={styles.navItems}>
+      <NavItem>Página Inicial</NavItem>
+      <NavItem>Categorias</NavItem>
+      <NavItem>Ajuda</NavItem>
+      <NavItem>Meus Agendamentos</NavItem>
+      <NavItem>Portal do Parceiro</NavItem>
+      <View style={styles.locationContainer}>
+        <Text style={styles.locationLabel}>Estou em:</Text>
         <TouchableOpacity style={styles.locationBox}>
-            {user?.location ?? 'Localização não definida'}
-          <Icon name="keyboard-arrow-down" size={18} color="#FFF" style={styles.arrowIcon} />
+          {user?.location ?? 'Localização não definida'}
+          <Icon
+            name="keyboard-arrow-down"
+            size={18}
+            color="#FFF"
+            style={styles.arrowIcon}
+          />
         </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        style={styles.userContainer}
-        activeOpacity={0.7}>
+      <TouchableOpacity style={styles.userContainer} activeOpacity={0.7}>
         <Image
           source={require('../../assets/logo.png')}
           style={styles.profileImage}
@@ -66,7 +66,7 @@ const AuthenticatedNav = () => {
         <Text style={styles.userName}>Douglas</Text>
       </TouchableOpacity>
     </View>
-)
+  );
 };
 
 const UnauthenticatedNav = () => (
