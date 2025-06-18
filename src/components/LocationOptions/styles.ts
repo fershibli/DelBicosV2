ea02f0e2b37da3b5d6a089ef16356f9cc8e0b5e0
isabelmaito <maitoisabel@gmail.com>
2025-06-18 15:14:00 -0300

    fix(styles): refine button styles for improved layout and consistency

--- src/components/LocationOptions/styles.ts
+++ src/components/LocationOptions/styles.ts
@@ -19,16 +19,29 @@ export const styles = StyleSheet.create({
   },
 
   button: {
+    gap: 10,
+    flexDirection: 'row',
     backgroundColor: '#005A93',
-    padding: 12,
+    paddingHorizontal: 12,
+    paddingTop: 4,
+    paddingBottom: 7,
     borderRadius: 60,
     marginVertical: 5,
     width: '100%',
     alignItems: 'center',
+    justifyContent: 'center',
   },
 
   buttonText: {
     color: '#ffffff',
-    fontWeight: 'bold',
+    fontSize: 32,
+    fontFamily: 'Afacad',
+    textAlign: 'center',
+  },
+
+  logoLocal: {
+    width: 24,
+    height: 24,
+    marginRight: 8, // Espaçamento entre o ícone e o texto
   },
 });
