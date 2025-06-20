460594535f3a05b14416773e549695d25fb75d5c
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-19 23:53:57 -0300

    fix(PhoneConfirmation): update phone number input handling and format for better user experience

--- src/screens/public/PhoneConfirmation/PhoneConfirmation.tsx
+++ src/screens/public/PhoneConfirmation/PhoneConfirmation.tsx
@@ -1,22 +1,16 @@
 import React, { useState } from 'react';
-import {
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  View,
-} from 'react-native';
+import { ScrollView, Text, TouchableOpacity, Alert, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { styles } from './styles';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { NavigationParams } from '../../types';
+import { MaskedTextInput } from 'react-native-mask-text';
 
 type NavigationProp = NativeStackNavigationProp<NavigationParams>;
 
 function PhoneConfirmationScreen() {
   const navigation = useNavigation<NavigationProp>();
-  const [phoneNumber, setPhoneNumber] = useState('(__) _____-____');
+  const [phoneNumber, setPhoneNumber] = useState('');
 
   const mockSendSMS = () => {
     const mockCode = '1234';
@@ -24,24 +18,8 @@ function PhoneConfirmationScreen() {
     Alert.alert('Simulação', 'Código 1234 enviado com sucesso!');
   };
 
-  const handlePhoneNumberChange = (text: string) => {
-    const cleanedText = text.replace(/\D/g, '').slice(2);
-    console.log('Número de telefone digitado:', cleanedText);
-    if (cleanedText.length <= 11) {
-      let formatedText = '(__) _____-____';
-      for (let i = 0; i < cleanedText.length; i++) {
-        formatedText = formatedText.replace('_', cleanedText[i]);
-      }
-      console.log('Número de telefone formatado:', formatedText);
-      setPhoneNumber(formatedText);
-    }
-  };
-
   const handleContinue = () => {
-    if (
-      phoneNumber &&
-      ('+55 ' + phoneNumber).match(/^\+\d{2}\s\(\d{2}\)\s\d{5}-\d{4}$/)
-    ) {
+    if (phoneNumber && phoneNumber.match(/^\+\d{2}\s\(\d{2}\)\s\d{5}-\d{4}$/)) {
       mockSendSMS();
     } else {
       Alert.alert(
@@ -49,6 +27,13 @@ function PhoneConfirmationScreen() {
         'Digite um número de telefone válido no formato +DD (XX) XXXXX-XXXX.',
       );
     }
+  };
+
+  const handlePhoneNumberChange = (text: string) => {
+    if (phoneNumber.length === 0) {
+      text = '+55 ' + text;
+    }
+    setPhoneNumber(text);
   };
 
   return (
@@ -61,10 +46,11 @@ function PhoneConfirmationScreen() {
           Insira seu celular para entrar. Você receberá um código SMS para
           confirmar seu telefone
         </Text>
-        <TextInput
+        <MaskedTextInput
+          mask="+99 (99) 99999-9999"
           style={styles.input}
           placeholder="DDD + CELULAR"
-          value={'+55 ' + phoneNumber}
+          value={phoneNumber}
           onChangeText={handlePhoneNumberChange}
           keyboardType="phone-pad"
         />
