e173361e7ed6c2d7f71db58d15e6f86e4d50cd3a
isabelmaito <maitoisabel@gmail.com>
2025-06-20 23:38:23 -0300

    feat(UserStore): implement signInPassword method for email/password authentication

--- src/stores/User/User.ts
+++ src/stores/User/User.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'expo-zustand-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { UserStore } from './types';
+import { backendHttpClient } from '@lib/helpers/httpClient';
 
 export const useUserStore = create<UserStore>()(
   persist(
@@ -16,6 +17,26 @@ export const useUserStore = create<UserStore>()(
           set({ user: mockedUser });
         } catch (error) {
           console.error('Error during login:', error);
+          return;
+        }
+      },
+      signInPassword: async (email: string, password: string) => {
+        try {
+          const response = await backendHttpClient.get('/user/login', {
+            params: {
+              email,
+              password,
+            },
+          });
+
+          if (response.status === 200) {
+            const user = response.data;
+            set({ user });
+          } else {
+            console.error('Login failed:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error during login with password:', error);
           return;
         }
       },
