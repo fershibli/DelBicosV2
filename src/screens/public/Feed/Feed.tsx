54d111631c61aa213c467b948f43af805168a245
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-20 22:32:27 -0300

    feat(FeedScreen): integrate ListProfessionals component into FeedScreen

--- src/screens/public/Feed/Feed.tsx
+++ src/screens/public/Feed/Feed.tsx
@@ -3,6 +3,7 @@ import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
 import { useLocation } from '@lib/hooks/LocationContext';
 import { styles } from './styles';
 import CategoryList from '@components/CategoryList';
+import ListProfessionals from '@components/ListProfessionals';
 
 function FeedScreen() {
   return (
@@ -10,6 +11,7 @@ function FeedScreen() {
       <Text style={styles.title}>Selecione por Categorias</Text>
       <CategoryList />
       <Text style={styles.title}>Profissionais próximos a você</Text>
+      <ListProfessionals />
     </View>
   );
 }
