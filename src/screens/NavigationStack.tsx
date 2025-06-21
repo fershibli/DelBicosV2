c60d56fa321898938078b82a8fc054d66a1eee1f
isabelmaito <maitoisabel@gmail.com>
2025-06-20 23:38:05 -0300

    feat(NavigationStack): add LoginPassword screen with linking path

--- src/screens/NavigationStack.tsx
+++ src/screens/NavigationStack.tsx
@@ -14,6 +14,7 @@ import { NavigationParams } from './types';
 import Login from './public/Login';
 import Header from '@components/Header';
 import { useUserStore } from '@stores/User';
+import { LoginPassword } from './public/LoginPassword';
 
 // If logged in Home = Feed, otherwise Home = Login
 const Home = () => {
@@ -33,6 +34,12 @@ const RootStack = createNativeStackNavigator<NavigationParams>({
       screen: Login,
       linking: {
         path: 'login',
+      },
+    },
+    LoginPassword: {
+      screen: LoginPassword,
+      linking: {
+        path: 'login-password',
       },
     },
     Feed: {
