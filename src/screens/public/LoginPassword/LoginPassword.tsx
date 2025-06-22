6b63d13539fcdcf2e896fac96e0e13d52fa5da78
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-22 02:31:11 -0300

    refactor(LoginPassword): remove unused useEffect import to clean up code

--- src/screens/public/LoginPassword/LoginPassword.tsx
+++ src/screens/public/LoginPassword/LoginPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, Text, Image, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Modal } from '@mui/material';
