fc23716f7601157f1980d8cd4ae3d374b1f93a50
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-22 14:42:19 -0300

    refactor(Category.ts): remove unused sleep function for cleaner code

--- src/stores/Category/Category.ts
+++ src/stores/Category/Category.ts
@@ -2,8 +2,6 @@ import { create } from 'zustand';
 import { Category, CategoryStore } from './types';
 import { backendHttpClient } from '@lib/helpers/httpClient';
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export const useCategoryStore = create<CategoryStore>()((set) => ({
   categories: [],
 
