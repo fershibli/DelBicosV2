5d37f6af7833a51d306afd98df98e4779cc072f6
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-22 14:39:34 -0300

    refactor(Category.ts): replace hardcoded categories with API fetch for dynamic data

--- src/stores/Category/Category.ts
+++ src/stores/Category/Category.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { Category, CategoryStore } from './types';
+import { backendHttpClient } from '@lib/helpers/httpClient';
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -8,46 +9,8 @@ export const useCategoryStore = create<CategoryStore>()((set) => ({
 
   fetchCategories: async () => {
     try {
-      const data: Category[] = [
-        {
-          id: 1,
-          title: 'Saúde & Bem-Estar',
-          description: 'Serviços relacionados à saúde e bem-estar.',
-          active: true,
-        },
-        {
-          id: 2,
-          title: 'Beleza & Estética',
-          description: 'Serviços de beleza e estética.',
-          active: true,
-        },
-        {
-          id: 3,
-          title: 'Reformas & Reparos',
-          description: 'Serviços de reformas e reparos em casa.',
-          active: true,
-        },
-        {
-          id: 4,
-          title: 'Serviços Gerais',
-          description: 'Serviços diversos para o dia a dia.',
-          active: true,
-        },
-        {
-          id: 5,
-          title: 'Serviços Domésticos',
-          description: 'Serviços relacionados a tarefas domésticas.',
-          active: true,
-        },
-        {
-          id: 6,
-          title: 'Pet',
-          description: 'Serviços relacionados a animais de estimação.',
-          active: true,
-        },
-      ];
-
-      await sleep(1000); // Simulate network delay
+      const response = await backendHttpClient.get('/api/categories');
+      const data: Category[] = response.data;
 
       set({ categories: data });
     } catch (error) {
