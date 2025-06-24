14325107ed8d772c55a49db74e14f3d7fd4c3b0a
douglaswenzel <douglas.wenzel@hotmail.com>
2025-06-23 21:17:09 -0300

    fix(types.ts): add Professional interface and its dependencies for better data structuring

--- src/screens/types.ts
+++ src/screens/types.ts
@@ -10,3 +10,59 @@ export type NavigationParams = {
   Register: undefined;
   NotFound: undefined;
 };
+
+export interface Professional {
+  id: number;
+  user_id: number;
+  main_address_id: number;
+  cpf: string;
+  cnpj: string;
+  createdAt: string;
+  updatedAt: string;
+  User: User;
+  address: Address;
+  services: any[];
+  amenities: any[];
+  gallery: GalleryImage[];
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  active: boolean;
+  avatarImg: string;
+  bannerImg: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Address {
+  id: number;
+  lat: string;
+  lng: string;
+  street: string;
+  number: string;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  country_iso: string;
+  postal_code: string;
+  user_id: number;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface GalleryImage {
+  id: number;
+  professional_id: number;
+  url: string;
+  description: string | null;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
