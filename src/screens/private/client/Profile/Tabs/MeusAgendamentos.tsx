600fbbf2b1b06b869e2fa2637497a5365275e893
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-29 15:52:10 -0300

    feat(MeusAgendamentos): implement MeusAgendamentos component for displaying user appointments
    Co-Authored-By: IagoYuriRossan <iagorossan321@gmail.com>
    Co-Authored-By: isabelmaito <maitoisabel@gmail.com>
    Co-Authored-By: konsanii <lucas_consani1@hotmail.com>
    Co-Authored-By: Eduardo Kamo <eduardokamoz@gmail.com>

---
+++ src/screens/private/client/Profile/Tabs/MeusAgendamentos.tsx
@@ -0,0 +1,34 @@
+import { useAppointmentStore } from '@stores/Appointment';
+import { Appointment } from '@stores/Appointment/types';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import AppointmentItem from '@components/AppointmentItem';
+import { Grid } from '@mui/material';
+
+function MeusAgendamentos() {
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
+
+  const { fetchAppointments } = useAppointmentStore();
+
+  useEffect(() => {
+    const loadAppointments = async () => {
+      const data = await fetchAppointments();
+      setAppointments(data);
+    };
+
+    loadAppointments();
+  }, []);
+
+  return (
+    <View style={{ gap: 10 }}>
+      <Text>Meus Agendamentos</Text>
+      <Grid container spacing={3}>
+        {appointments.map((appointment) => (
+          <AppointmentItem key={appointment.id} appointment={appointment} />
+        ))}
+      </Grid>
+    </View>
+  );
+}
+
+export default MeusAgendamentos;
