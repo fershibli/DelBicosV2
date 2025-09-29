e47ce9f56af1d3c390076f053083561c919d3b5d
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-29 15:51:29 -0300

    fix(colors): restore primaryGreen color definition in theme
    Co-Authored-By: IagoYuriRossan <iagorossan321@gmail.com>
    Co-Authored-By: isabelmaito <maitoisabel@gmail.com>
    Co-Authored-By: konsanii <lucas_consani1@hotmail.com>
    Co-Authored-By: Eduardo Kamo <eduardokamoz@gmail.com>

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,6 +2,7 @@ const colors = {
   primaryBlue: '#005A93',
   primaryOrange: '#FC8200',
   primaryBlack: '#000000',
+  primaryGreen: '#22843B',
   primaryWhite: '#FFFFFF',
   secondaryGray: '#DDE6F0',
   secondaryBeige: '#E0E0E0',
