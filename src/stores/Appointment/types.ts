1c07b88f835143655f504be539cba41484e62bb9
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-29 02:06:05 -0300

    feat(AppointmentStore): implement appointment fetching logic and define types
    Co-Authored-By: IagoYuriRossan <iagorossan321@gmail.com>
    Co-Authored-By: isabelmaito <maitoisabel@gmail.com>
    Co-Authored-By: konsanii <lucas_consani1@hotmail.com>
    Co-Authored-By: Eduardo Kamo <eduardokamoz@gmail.com>

---
+++ src/stores/Appointment/types.ts
@@ -0,0 +1,70 @@
+export interface Service {
+  id: number;
+  title: string;
+  description: string;
+  price: string;
+  duration: number; // duration in minutes
+  banner_uri: string | null;
+  active: boolean;
+  subcategory_id: number;
+  professional_id: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  active: boolean;
+  avatar_uri: string | null;
+  banner_uri: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Client {
+  id: number;
+  user_id: number;
+  main_address_id: number;
+  cpf: string;
+  createdAt: string;
+  updatedAt: string;
+  User: User;
+}
+
+export interface Professional {
+  id: number;
+  user_id: number;
+  main_address_id: number;
+  cpf: string;
+  cnpj: string | null;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  User: User;
+}
+
+export interface Appointment {
+  id: number;
+  professional_id: number;
+  client_id: number;
+  service_id: number;
+  address_id: number;
+  rating: number | null;
+  review: string | null;
+  start_time: string; // ISO date string
+  end_time: string; // ISO date string
+  status: 'pending' | 'confirmed' | 'completed' | 'canceled';
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+  Service: Service;
+  Client: Client;
+  Professional: Professional;
+}
+
+export interface AppointmentStore {
+  fetchAppointments: () => Promise<Appointment[]>;
+}
