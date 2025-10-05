ae3e1a9067465906491628159d0d79e3381cfb34
Eduardo Kamo <eduardokamoz@gmail.com>
2025-10-05 20:27:10 -0300

    style(DateInput.tsx)- style adjusted

--- src/components/DateInput/styles.ts
+++ src/components/DateInput/styles.ts
@@ -1,15 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    marginBottom: 10,
-  },
   input: {
-    backgroundColor: '#ffffff',
+    backgroundColor: '#FFFFFF',
     borderRadius: 8,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
+    paddingHorizontal: 16,
+    height: 50,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#CED4DA',
+    fontFamily: 'Afacad-Regular',
+    color: '#212529',
     width: '100%',
+  },
+  inputError: {
+    borderColor: '#D32F2F',
+    borderWidth: 1.5,
   },
 });
