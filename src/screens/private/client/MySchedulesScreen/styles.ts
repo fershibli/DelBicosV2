f03af284ea758872c45ae45de85b98783fc284a0
Eduardo Kamo <eduardokamoz@gmail.com>
2025-10-29 16:58:01 -0300

    feat: remove scrollview from my schedules screen

--- src/screens/private/client/MySchedulesScreen/styles.ts
+++ src/screens/private/client/MySchedulesScreen/styles.ts
@@ -3,15 +3,6 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F4F7FA', // Mesmo fundo do perfil
-  },
-  contentContainer: {
-    padding: 20,
-  },
-  pageTitle: {
-    fontSize: 28,
-    fontFamily: 'Afacad-Bold',
-    color: '#003366',
-    marginBottom: 24,
+    backgroundColor: '#F4F7FA',
   },
 });
