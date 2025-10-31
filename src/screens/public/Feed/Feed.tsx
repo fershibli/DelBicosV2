f8bb6121a5d7808e8877d1d0aed1a277539ba3b3
Eduardo Kamo <eduardokamoz@gmail.com>
2025-10-31 05:33:29 -0300

    docs: remove unused address selection modal import

--- src/screens/public/Feed/Feed.tsx
+++ src/screens/public/Feed/Feed.tsx
@@ -3,6 +3,7 @@ import { Text, ScrollView } from 'react-native';
 import { styles } from './styles';
 import CategorySlider from '@components/CategorySlider';
 import ListProfessionals from '@components/ListProfessionals';
+// import AddressSelectionModal from '@components/AddressSelectionModal';
 
 const FeedScreen: React.FC = () => {
   return (
@@ -11,6 +12,12 @@ const FeedScreen: React.FC = () => {
       contentContainerStyle={styles.contentContainer}>
       <Text style={styles.title}>Selecione por Categorias</Text>
       <CategorySlider />
+      {/* <AddressSelectionModal
+        visible={true}
+        onClose={() => {}}
+        userId="1"
+        onAddressSelect={() => {}}
+      /> */}
       <Text style={styles.title}>Profissionais próximos a você</Text>
       <ListProfessionals />
     </ScrollView>
