1281268bca15371cace38d52a6917f14023ccd94
IagoYuriRossan <iagorossan321@gmail.com>
2025-11-21 17:38:52 -0300

    feat(HighlightCard): refactor to use dynamic theme colors for styling

--- src/components/ui/HighlightCard/styles.ts
+++ src/components/ui/HighlightCard/styles.ts
@@ -1,40 +1,40 @@
 import { StyleSheet, Dimensions } from 'react-native';
-import colors from '@theme/colors';
 
 const { width } = Dimensions.get('window');
 const CARD_WIDTH = width;
 const CARD_HEIGHT = 250;
 
-export const styles = StyleSheet.create({
-  card: {
-    width: CARD_WIDTH,
-    height: CARD_HEIGHT,
-    borderRadius: 0,
-    marginHorizontal: 0,
-    backgroundColor: colors.secondaryBeige,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  imageStyle: {
-    borderRadius: 0,
-  },
-  gradient: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    padding: 24,
-    borderRadius: 0,
-  },
-  title: {
-    fontFamily: 'Afacad-Bold',
-    fontSize: 24,
-    color: '#E2E8F0',
-    marginBottom: 4,
-  },
-  description: {
-    fontFamily: 'Afacad-Regular',
-    fontSize: 16,
-    color: '#E2E8F0',
-  },
-});
+export const createStyles = (colors: any) =>
+  StyleSheet.create({
+    card: {
+      width: CARD_WIDTH,
+      height: CARD_HEIGHT,
+      borderRadius: 0,
+      marginHorizontal: 0,
+      backgroundColor: colors.secondaryBeige,
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'flex-end',
+    },
+    imageStyle: {
+      borderRadius: 0,
+    },
+    gradient: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      padding: 24,
+      borderRadius: 0,
+    },
+    title: {
+      fontFamily: 'Afacad-Bold',
+      fontSize: 24,
+      color: '#E2E8F0',
+      marginBottom: 4,
+    },
+    description: {
+      fontFamily: 'Afacad-Regular',
+      fontSize: 16,
+      color: '#E2E8F0',
+    },
+  });
