b6fe81ff87ae66b6a83dda71fcdf2de7742ee7d8
isabelmaito <maitoisabel@gmail.com>
2026-05-18 18:31:52 -0300

    feat: update web domain configuration in environment variables

--- src/config/varEnvs.ts
+++ src/config/varEnvs.ts
@@ -2,6 +2,7 @@ import { Platform } from 'react-native';
 
 export const ENVIRONMENT: 'development' | 'staging' | 'production' =
   process.env.EXPO_PUBLIC_ENVIRONMENT;
+const DOMAIN_WEB = process.env.EXPO_PUBLIC_DOMAIN_WEB;
 const DOMAIN_ANDROID = process.env.EXPO_PUBLIC_DOMAIN_ANDROID;
 const DOMAIN_IOS = process.env.EXPO_PUBLIC_DOMAIN_IOS;
 const PORT = process.env.EXPO_PUBLIC_PORT;
@@ -11,7 +12,7 @@ const WS_PROTOCOL = process.env.EXPO_PUBLIC_WS_PROTOCOL;
 const localhost = Platform.select({
   android: DOMAIN_ANDROID,
   ios: DOMAIN_IOS,
-  web: DOMAIN_ANDROID,
+  web: DOMAIN_WEB,
 });
 
 const DOMAIN = PORT ? `${localhost}:${PORT}` : `${localhost}`;
