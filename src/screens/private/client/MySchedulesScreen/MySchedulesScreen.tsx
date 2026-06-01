72e3ceca1c02bd1ffd7577ae3f64e4dd404addfe
isabelmaito <maitoisabel@gmail.com>
2026-05-31 21:06:28 -0300

    fix: update MySchedulesScreen styles for dark mode and high contrast support

--- src/screens/private/client/MySchedulesScreen/MySchedulesScreen.tsx
+++ src/screens/private/client/MySchedulesScreen/MySchedulesScreen.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 import { createStyles } from './styles';
 import { useColors } from '@theme/ThemeProvider';
+import { useThemeStore, ThemeMode } from '@stores/Theme';
 import MeusAgendamentos from '../Profile/Tabs/MeusAgendamentos/MeusAgendamentos';
 
 const MySchedulesScreen: React.FC = () => {
   const colors = useColors();
-  const styles = createStyles(colors);
+  const { theme } = useThemeStore();
+  const isDark = theme === ThemeMode.DARK;
+  const isHighContrast = theme === ThemeMode.LIGHT_HI_CONTRAST;
+  const styles = createStyles(colors, isDark, isHighContrast);
 
   return (
     <View style={styles.container}>
