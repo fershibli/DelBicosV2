9be9d6a5a160d25432c1fc8412983e60fda4b0b2
IagoYuriRossan <iagorossan321@gmail.com>
2026-07-06 11:30:11 -0300

    chore: update src/screens/navigationRef.ts

--- src/screens/navigationRef.ts
+++ src/screens/navigationRef.ts
@@ -2,3 +2,11 @@ import { createNavigationContainerRef } from '@react-navigation/native';
 import { NavigationParams } from './types';
 
 export const navigationRef = createNavigationContainerRef<NavigationParams>();
+
+export function navigate(name: keyof NavigationParams | string, params?: any) {
+  if (navigationRef.isReady()) {
+    // @ts-ignore
+    navigationRef.navigate(name, params);
+  }
+}
+
