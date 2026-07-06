b48f9842bcdf64f89dbbb4c94c452b9e7098946e
IagoYuriRossan <iagorossan321@gmail.com>
2026-07-06 11:30:18 -0300

    chore: update src/screens/types.ts

--- src/screens/types.ts
+++ src/screens/types.ts
@@ -40,11 +40,12 @@ export type NavigationParams = {
   SearchResult: { subCategoryId: number; date: string };
   Checkout: {
     professionalId: number;
-    priceFrom: number;
+    priceFrom?: number;
     selectedTime: string;
     imageUrl?: string;
     professionalName?: string;
     serviceId: number;
+    appointmentId?: number;
   };
   PaymentStatus:
     | {
