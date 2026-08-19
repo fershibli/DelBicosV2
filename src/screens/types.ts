ef339e794ff9773c0cb3601ae25e9ee75c44f726
IagoYuriRossan <iagorossan321@gmail.com>
2026-08-19 14:58:57 -0300

    feat: update SearchResult type to support query parameter

--- src/screens/types.ts
+++ src/screens/types.ts
@@ -37,7 +37,7 @@ export type NavigationParams = {
     singleSubCategory?: { id: number; title: string };
   };
   ClientProfile: { subroute?: ClientProfileSubRoutes };
-  SearchResult: { subCategoryId: number; date: string };
+  SearchResult: { subCategoryId: number; date: string } | { query: string };
   Checkout: {
     professionalId: number;
     priceFrom?: number;
@@ -48,11 +48,11 @@ export type NavigationParams = {
     appointmentId?: number;
   };
   PaymentStatus:
-  | {
-    appointmentId?: number;
-    paymentIntentId?: string;
-  }
-  | undefined;
+    | {
+        appointmentId?: number;
+        paymentIntentId?: string;
+      }
+    | undefined;
   MySchedules: undefined;
   ProfessionalTabs: undefined;
   ProfessionalHomeTab: undefined;
